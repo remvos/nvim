a6d826e36a6f4ffca696b6cd488c65f5ad009453
Remco Vossen <remco@remvos.nl>
2025-01-29 10:56:47 +0100

    autocommit

--- lua/custom/plugins/nerdtree.lua
+++ lua/custom/plugins/nerdtree.lua
@@ -3,5 +3,12 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>n', ':NERDTreeFocus<CR>', { desc = 'Focus NERDTree' })
     vim.keymap.set('n', '<leader>N', ':NERDTreeToggle<CR>', { desc = 'Toggle NERDTree' })
+
+    -- Start NERDTree and put the cursor back in the other window.
+    --vim.api.nvim_create_autocmd('VimEnter', {
+    --  callback = function()
+    --    vim.api.eval ':NERDTREE | windcmd p'
+    --  end,
+    --})
   end,
 }
--- lua/custom/plugins/remco.lua
+++ lua/custom/plugins/remco.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<leader>t', 'O* [ ] ', { desc = 'Insert to-do item.' })
+vim.keymap.set('n', '<BS>', ':bprev<CR>', { desc = 'Switch to previous buffer.' })
 
 return {}
